9d26bb8194b3c8d71c66c3383d47752a4801b8ff
maruf89541 <maruf89541@gmail.com>
2021-10-18 01:56:48 +0600

    hello

---
+++ data.js
@@ -0,0 +1,58 @@
+const data = [
+    {
+        id: "01",
+        name: "Get ambulance",
+        short_d: "",
+        img: "",
+        descripiton: "",
+        rating: "03",
+
+    },
+    {
+        id: "01",
+        name: "Get ambulance",
+        short_d: "",
+        img: "",
+        descripiton: "",
+        rating: "03",
+
+    },
+    {
+        id: "01",
+        name: "Get ambulance",
+        short_d: "",
+        img: "",
+        descripiton: "",
+        rating: "03",
+
+    },
+    {
+        id: "01",
+        name: "Get ambulance",
+        short_d: "",
+        img: "",
+        descripiton: "",
+        rating: "03",
+
+    },
+    {
+        id: "01",
+        name: "Get ambulance",
+        short_d: "",
+        img: "",
+        descripiton: "",
+        rating: "03",
+
+    },
+    {
+        id: "01",
+        name: "Get ambulance",
+        short_d: "",
+        img: "",
+        descripiton: "",
+        rating: "03",
+
+    },
+]
+JSON.stringify(data)
+copy(JSON.stringify(data))
